8df23708bba494016d2c009e328d6a8e9c80af7d
Victor Aprea <victor.aprea@wickeddevice.com>
2017-04-15 18:30:59 -0400

    have to call Wire.begin() in setup

--- BME680_Library/examples/Basic/Basic.ino
+++ BME680_Library/examples/Basic/Basic.ino
@@ -1,9 +1,12 @@
 #include <BME680_Library.h>
+#include <Wire.h>
 
 BME680_Library bme680;
 
 void setup(void){
   Serial.begin(9600);
+
+  Wire.begin();
 
   Serial.print("BME Initialization...");
   if(bme680.begin()){
